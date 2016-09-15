a5519428ee2fd0c2d9ec3049b155e4c67fb558e0
AlexAegis <alexaegis@gmail.com>
2016-09-15 17:38:58 +0200

    homework added

---
+++ lesson01/src/main/java/com/github/alexaegis/homework/Main.java
@@ -0,0 +1,11 @@
+package com.github.alexaegis.homework;
+
+/**
+ * Készíts egy stacket mely karaktereket tartalmaz és legyen benne metódus
+ * mely leellenőrzi, hogy helyesen van e zárójelezve.
+ * A verem olvasáskor ürüljön ki.
+ */
+
+public class Main {
+
+}
