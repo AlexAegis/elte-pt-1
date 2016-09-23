55a77851c333706688116b062200f0273c8c7d7e
AlexAegis <alexaegis@gmail.com>
2016-09-23 23:12:01 +0200

    equals and hashcode implemented

--- homework02/src/main/java/com/github/alexaegis/shop/Product.java
+++ homework02/src/main/java/com/github/alexaegis/shop/Product.java
@@ -26,4 +26,30 @@ public abstract class Product {
 		this.price = price;
 		return this;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+
+		Product product = (Product) o;
+
+		if (price != product.price) {
+			return false;
+		}
+
+		return name != null ? name.equals(product.name) : product.name == null;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = name != null ? name.hashCode() : 0;
+		result = 31 * result + price;
+		return result;
+	}
 }
