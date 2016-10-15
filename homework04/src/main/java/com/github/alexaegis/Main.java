f635e16be037ae4852d8f0f2543e84243a1386e2
AlexAegis <alexaegis@gmail.com>
2016-10-15 03:34:15 +0200

    cleaning up

--- homework04/src/main/java/com/github/alexaegis/Main.java
+++ homework04/src/main/java/com/github/alexaegis/Main.java
@@ -11,23 +11,19 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Student student1 = new Student("VIKTOR").addGrades(1,2,1,2,3,2);
-        Student student2 = new Student("LORNCZ").addGrades(2,2,1,2,3,2,1);
-        Student student3 = new Student("HLKPTR").addGrades(1,2,1,1,1,1,3);
-        Student student4 = new Student("LAZAR1").addGrades(2,3,4,1,2,1);
         List<Student> students = new ArrayList<>();
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
+        students.add(new Student("VIKTOR",1,2,1,2,3,2));
+        students.add(new Student("LORNCZ",2,2,1,2,3,2,1));
+        students.add(new Student("HLKPTR",1,2,1,1,1,1,3));
+        students.add(new Student("LAZAR1",2,3,4,1,2,1));
         System.out.println("Igaz e, hogy minden tanulónak az átlaga legalább négyes? "
-                + Student.aboveAverageStudents(4, student1,student2,student3,student4));
+                + Student.isAllAboveAverage(4, students));
         System.out.println("A legjobb 3 átlaggal rendelkező: ");
         Student.showBestNOf(3, students);
 
         List<Student> studentsFromFile = new StudentReader(new File("src\\main\\resources\\positiveinput.txt")).getStudents();
         System.out.println("Igaz e, hogy minden tanulónak az átlaga legalább négyes? "
-                + Student.aboveAverageStudents(4, studentsFromFile));
+                + Student.isAllAboveAverage(4, studentsFromFile));
         System.out.println("A legjobb 3 átlaggal rendelkező: ");
         Student.showBestNOf(3, studentsFromFile);
     }
